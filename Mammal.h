71c561eef8eb509a4caa5c5ff0df47e8d58637f2
Abdelrahman Rushdy <aboudyrushdy25@gmail.com>
2025-11-12 15:01:11 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,7 +3,27 @@
 
 #include "Animal.h"
 
-//define mammal class here
+class Mammal::Animal{
+        private:
+        string furColor;
+
+        public:
+        Mammal(string name, int age, bool isHungry, string furColor){
+            this->furColor = furColor;
+        }
+        
+        void setfurColor(string setfurColor){
+            setfurColor = furColor;
+        }
+
+        string getfurColor(){
+            return furColor;
+        }
+
+        ~Mammal(){
+
+        }
+    };
 
 
 
