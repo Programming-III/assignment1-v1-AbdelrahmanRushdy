005d1474c3e4adcdbaceebb4073a82afdaa2aae2
Abdelrahman Rushdy <aboudyrushdy25@gmail.com>
2025-11-12 15:02:59 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -2,13 +2,29 @@
 #define BIRD_H
 
 #include "Animal.h"
+#include <iostream>
+#include <string>
 
-//define class bird here 
+class Bird::Animal{
+        private:
+        float wingSpan;
 
+        public:
+        Bird(string name, int age, bool isHungry, float wingSpan){
+            this->wingSpan = wingSpan;
+        }
 
+        void setwingSpan(float setWingSpan){
+            setWingSpan = wingSpan;
+        }
 
+        float getwingSpan(){
+            return wingSpan;
+        }
 
+        ~Bird(){
 
-
+        }
+};
 
 #endif
