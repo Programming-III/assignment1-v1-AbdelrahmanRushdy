d3c9ee03094e26226d4e21473456c105e3f4bf71
Abdelrahman Rushdy <aboudyrushdy25@gmail.com>
2025-11-12 15:10:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,13 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
-    
+    Enclosure e1(1, 10);
+    e1.addAnimal(new Mammal("Lion", 5, true, "Golden"));
+    e1.addAnimal(new Bird("Parrot", 2, false, 0.25f));
+    e1.addAnimal(new Reptile("Snake", 3, true, true));
+    e1.displayAnimals();
+    Visitor v("Sarah Ali", 3);
+    v.displayInfo();
     
     return 0;
 }
